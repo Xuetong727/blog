aa5c81f9af07fafede2235ecdca034bdaf2de381
Xuetong727 <980954908@qq.com>
2023-11-13 20:07:18 +0800

    1113

--- src/main/java/com/liuwenhao/myblog/domain/Article.java
+++ src/main/java/com/liuwenhao/myblog/domain/Article.java
@@ -1,4 +1,27 @@
 package com.liuwenhao.myblog.domain;
 
+import lombok.Data;
+
+@Data
 public class Article {
+
+    private Integer id;
+
+    private Integer commentCounts;
+
+    private Integer createDate;
+
+    private String summary;
+
+    private String title;
+
+    private Integer viewCounts;
+
+    private Integer weight;
+
+    private Integer AuthorId;
+
+    private Integer bodyId;
+
+    private Integer categoryId;
 }
